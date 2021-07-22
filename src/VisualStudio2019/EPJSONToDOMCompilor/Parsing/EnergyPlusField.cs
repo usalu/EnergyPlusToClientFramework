d1cb90dc0ea9fdbdbc50704ce2107aa534e1c785
Ueli <u.saluz@campus.tu-berlin.de>
2021-07-22 02:05:21 +0200

    Reuse of SemIO compilor; Setting up first parser models for compilor

---
+++ src/VisualStudio2019/EPJSONToDOMCompilor/Parsing/EnergyPlusField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPJSONToDOMCompilor.Parsing
+{
+    class EnergyPlusField
+    {
+        public string Name { get; set; }
+
+        public EnergyPlusFieldType FieldType { get; set; }
+
+        
+    }
+}
---
+++ src/VisualStudio2019/EPJSONToDOMCompilor/Parsing/EnergyPlusFieldType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPJSONToDOMCompilor.Parsing
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    enum EnergyPlusFieldType
+    {
+        EnergyPlusName,
+        EnergyPlusNumber,
+        EnergyPlusAutosizableNumber,
+        EnergyPlusType,
+        EnergyPlusArray
+    }
+}
