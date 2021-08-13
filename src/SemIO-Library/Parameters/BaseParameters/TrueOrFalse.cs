5cd43df5eb0366d27a48ff9292394ea6e5e8ca28
Ueli <u.saluz@campus.tu-berlin.de>
2021-08-13 12:03:47 +0200

    Added the IDD and EPJSON-Schema to the input and first compilation of the schema.
    The first result with anonymous classes and dictionaries is not very usable for humans. Further structuring will be necessary.

---
+++ src/SemIO-Library/Parameters/BaseParameters/TrueOrFalse.cs
@@ -0,0 +1,16 @@
+﻿namespace SemIOLibrary.Parameters.BaseParameters
+{
+    public class TrueOrFalse : Parameter
+    {
+        public bool Value { get; set; }
+
+        public TrueOrFalse()
+        {
+        }
+
+        public TrueOrFalse(bool value)
+        {
+            Value = value;
+        }
+    }
+}
